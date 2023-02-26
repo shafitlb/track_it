788a07b8e66b1d9d7ff9e2291e902b94e584a632
shafitlb <76275404+shafitlb@users.noreply.github.com>
2023-02-26 20:15:00 +0530

    second push

--- lib/addItem.dart
+++ lib/addItem.dart
@@ -9,27 +9,92 @@ class AddItem extends StatefulWidget {
   State<AddItem> createState() => _AddItemState();
 }
 
-// ignore: camel_case_types
 class _AddItemState extends State<AddItem> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.all(50),
-            // padding: EdgeInsets.all(20),
-            width: MediaQuery.of(context).size.width,
-            height: 200,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("asset/images/wallet.jpg"),
-                fit: BoxFit.cover,
-              ),
-            ),
-          )
-        ],
-      ),
-    );
+        appBar: AppBar(
+          leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
+          title: Text("Add Items"),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(16),
+          child: Form(
+              key: _formKey,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Lost Item *',
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Name required';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      maxLines: 5,
+                      textInputAction: TextInputAction.next,
+                      decoration: const InputDecoration(
+                        labelText: 'Description',
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Description required';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(border: Border.all()),
+                        ),
+                        SizedBox(
+                          width: 100,
+                        ),
+                        IconButton(
+                            icon: const Icon(Icons.image_outlined),
+                            onPressed: () {}),
+                        IconButton(
+                            icon: const Icon(Icons.camera_alt_outlined),
+                            onPressed: () {
+                              // _controller.remove(index);
+                            }),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 150,
+                    ),
+                    SizedBox(
+                      width: 100,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            primary: const Color.fromARGB(255, 7, 182, 241),
+                            shape: const StadiumBorder()),
+                        child: const Text(
+                          'Save',
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )),
+        ));
   }
 }
--- lib/details.dart
+++ lib/details.dart
@@ -9,12 +9,47 @@ class Details extends StatefulWidget {
   State<Details> createState() => _DetailsState();
 }
 
+// ignore: camel_case_types
 class _DetailsState extends State<Details> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("data"),
+      appBar: AppBar(
+        leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
+        title: Text("Details"),
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.all(50),
+            // padding: EdgeInsets.all(20),
+            width: MediaQuery.of(context).size.width,
+            height: 200,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("asset/images/wallet.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+              child: Text(
+            "Wallet",
+            style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic),
+          )),
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Text(
+                "Wallet is placed in the living room side drawyer, hhdifjuhsihgdfiuagshdfiaifagsdfgadfgdagfgahdfgihfdiuhfiuhdhfihdsaiufhasidufhiudsfiudshfiuhdfh"),
+          )
+        ],
       ),
     );
   }
